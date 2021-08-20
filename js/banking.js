4c5f924f49dc81d04fa3f3dc22b0fffe5bbcd178
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-20 17:09:57 +0600

    banking.html updated with adding withdraw

--- js/banking.js
+++ js/banking.js
@@ -20,4 +20,31 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotal.innerText = newBalanceTotal;
 
+    // handle withdraw event handler 
+    document.getElementById('withdraw-button').addEventListener('click', function () {
+        // get withdraw amount 
+        const withdrawInput = document.getElementById('withdraw-input');
+        const withdrawAmountText = withdrawInput.value;
+        const withdrawAmount = parseFloat(withdrawAmountText);
+        // set withdraw amount 
+        const withdrawTotal = document.getElementById('withdraw-total');
+        const previousWithdrawTotalText = withdrawTotal.innerText;
+        const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+        // add previous and current withdraw 
+        const newWithdrawTotal = previousWithdrawTotal + withdrawAmount;
+        withdrawTotal.innerText = newWithdrawTotal;
+
+        // update account balance 
+        const balanceTotal = document.getElementById('balance-total');
+        const previousBalanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(previousBalanceTotalText);
+
+        const newBalanceTotal = previousBalanceTotal - withdrawAmount;
+        balanceTotal.innerText = newBalanceTotal;
+
+        // clear withdraw input 
+        withdrawInput.value = '';
+
+    });
+
 });
