3eaa732e721d617b0bf840a6d50b0e6838b98d24
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-20 12:00:25 +0600

    banking.html updated with grid

--- js/bank.js
+++ js/bank.js
@@ -1,10 +1,13 @@
 document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email 
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
     console.log(email);
+    // get user password 
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
     console.log(password);
+    // check email and password 
     if (email == 'sontan@baap.com' && password == 'secret') {
         console.log('Valid')
         window.location.href = 'banking.html';
